39fb9765649d9315f695f2a94811239d8cbca0a4
shubhamsanodiya2805 <shubham@my.veravalonline.com>
2026-08-16 22:23:55 +0530

    setup backend utilities

---
+++ Server/src/utils/response.js
@@ -0,0 +1,16 @@
+export const successResponse=(res,statusCode,message,data=null)=>{
+    return res.status(statusCode).json({
+        success:true,
+        message,
+        data
+    });
+};
+
+export const errorResponse=(res,statusCode,message,error=null)=>{
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        error
+    })
+};
+
